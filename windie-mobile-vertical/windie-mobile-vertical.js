9ae8382cddb5ea4197b48c9e8019b105114b3d9b
K1mjueun <jueun4779@gmail.com>
2022-06-27 12:09:21 +0900

    start page 작업 후 js, login page 추가

---
+++ windie-mobile-vertical/windie-mobile-vertical.js
@@ -0,0 +1,27 @@
+
+//화면 전환
+let displayAppear = [
+  {opacity: 0.7},
+  {opacity: 0.8},
+  {opacity: 0.9},
+  {opacity: 1},
+];
+
+  let displayChangeTime = {
+    duration: 500,
+    iterations: 1
+  };
+
+  function displayChange(element, disappearTarget, appearTarget) {
+    element.addEventListener('click', function() {
+      disappearTarget.style.display = "none";
+      appearTarget.style.display = "flex";
+      appearTarget.animate(displayAppear,displayChangeTime);
+      appearTarget.style.opacity = 1;
+    });
+  };
+
+  const startP = document.querySelector("#start-page");
+  const startPlogo = startP.children;
+  const loginP = document.querySelector("#login-page");
+  displayChange(startPlogo[0],startP,loginP);
